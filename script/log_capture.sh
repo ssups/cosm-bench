6e95bc4106bff1043448b1fcfae0c7db12b75558
ssups <shalstlqruki@gmail.com>
2025-01-25 14:49:05 +0900

    chore: log_capture exit

--- script/log_capture.sh
+++ script/log_capture.sh
@@ -3,11 +3,13 @@ NODE_NUM=$1
 echo "Starting log capture for node ${NODE_NUM}" >&2  
 
 while IFS= read -r line; do      
+    if [[ -z "$line" ]]; then  
+        break  
+    fi  
+
     if echo "$line" | grep -q "committed state"; then  
         clean_line=$(echo "$line" | sed -r "s/\x1B\[[0-9;]*[mK]//g")  
         timestamp=$(perl -MTime::HiRes=time -e 'printf "%d",time()*1000')  
         echo "$timestamp $clean_line" >> "./logs/output${NODE_NUM}.log"  
     fi  
 done  
-
-echo "Log capture ended for node ${NODE_NUM}" >&2  
